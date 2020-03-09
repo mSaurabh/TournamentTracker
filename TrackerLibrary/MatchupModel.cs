99a8727baa9ba17ad9b6135264f4b1f5fb21175a
Saurabh Mankar <Saurabh.Mankar@OptimaHCS.com>
2020-03-09 11:57:28 -0400

    Add project files.

---
+++ TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        /// <summary>
+        /// Represents 
+        /// </summary>
+        public List<MatchupEntryModel> Entries { get; set; }
+        public TeamModel Winner { get; set; }
+        public int MatchupRound { get; set; }
+
+    }
+}
