499da9e4e9bb75f0e36a82b40bf2f191cba68a41
SM-OfficeLaptop-GitHubApp <mankar.saurabh@gmail.com>
2020-12-01 10:18:26 -0500

    After Completing Video 3 of the ASP.Net Addon Course

--- MVCUI/Controllers/HomeController.cs
+++ MVCUI/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrackerLibrary;
+using TrackerLibrary.Models;
 
 namespace MVCUI.Controllers
 {
@@ -10,21 +12,10 @@ namespace MVCUI.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            List<TournamentModel> tournaments = GlobalConfig.Connection.GetTournament_All();
+
+            return View(tournaments);
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
